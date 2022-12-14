c35bfd0c5533a90ed2172215532de9b88913101e
Anna200787 <a.arutynyan@gmail.com>
2022-12-15 02:12:23 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog=8.0;
+        System.out.println(dog);
+        var cat =3.6;
+        System.out.println(cat);
+        var paper=763789;
+        System.out.println(paper);
+        dog=8.0+4;
+        System.out.println(dog);
+        cat=3.6+4;
+        System.out.println(cat);
+        paper=763789+4;
+        System.out.println(paper);
+        dog=8.0-3.5;
+        System.out.println(dog);
+        cat=cat-1.6;
+        System.out.println(cat);
+        paper=paper-7639;
+        System.out.println(paper);
+        var friend=19;
+        System.out.println(friend);
+        friend=friend+2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+        var weightFirstBoxer=78.2;
+        var weightSecondBoxer=82.7;
+        var totalWeight=weightFirstBoxer+weightSecondBoxer;
+        System.out.println("Общий вес боксеров " + totalWeight + " кг");
+        var weightDifference=weightFirstBoxer-weightSecondBoxer;
+        System.out.println("Разница в весе боксеров составляет " +weightDifference + "кг");
+        var weightDifference2=weightSecondBoxer-weightFirstBoxer;
+        System.out.println("Второй боксер больше первого боксера на " +weightDifference2 + " кг");
+        var weightDifference3=(weightSecondBoxer-weightFirstBoxer)%weightSecondBoxer;
+        System.out.println("Первый боксер меньше второго боксера на " + weightDifference3 + " кг");
+        var workingHours=8;
+        var allWorkingHours=640;
+        var numberEmployees=allWorkingHours/workingHours;
+        System.out.println("Всего работников в компании - " + numberEmployees + " человек");
+        var moreNumberEmployees=numberEmployees+94;
+        var moreAllWorkingHours=moreNumberEmployees*workingHours;
+        System.out.println("Если в компании работает " + moreNumberEmployees + " человека, то всего " + moreAllWorkingHours + " часов работы может быть поделено между сотрудниками.");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
